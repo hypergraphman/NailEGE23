8f9b1b704e2bedf1c8b7ef62f5078f77a8c8ad70
Руслан Аитов <r_aitov@mail.ru>
2022-09-09 15:21:04 +0300

    Initial commit

---
+++ task2/r22.py
@@ -0,0 +1,17 @@
+print('x y z w')
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                f = int((x or y) and (y != z) and not w)
+                if f:
+                    print(x, y, z, w)
+print()
+
+from itertools import product
+
+print('x y z w')
+for x, y, z, w in product((0, 1), repeat=4):
+    f = int((x or y) and (y != z) and not w)
+    if f:
+        print(x, y, z, w)
