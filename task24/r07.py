a649e0f2d197a49808cf47057e65f06a0040c946
Руслан Аитов <r_aitov@mail.ru>
2023-03-20 17:18:14 +0300

    task 24

---
+++ task24/r07.py
@@ -0,0 +1,14 @@
+s = open('24.txt').read().strip()
+cur_len = 1
+mx_len = 1
+i_end_mx_line = 0
+for i in range(1, len(s)):
+    if s[i - 1] != s[i]:
+        cur_len += 1
+        if cur_len > mx_len:
+            mx_len = cur_len
+            i_end_mx_line = i
+    else:
+        cur_len = 1
+print(mx_len)
+print(s[i_end_mx_line - mx_len + 1: i_end_mx_line + 1])
